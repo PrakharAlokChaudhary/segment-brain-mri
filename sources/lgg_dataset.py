c00f6cf524de9ec0ef3aa4a650406cd0cb6530ce
PrakharAlokChaudhary <78715364+PrakharAlokChaudhary@users.noreply.github.com>
2021-10-16 13:18:10 +0530

    Update lgg_dataset.py

--- sources/lgg_dataset.py
+++ sources/lgg_dataset.py
@@ -1,3 +1,4 @@
+#lgg_dataset.py
 from skimage import io
 import torch
 from torch.utils.data import Dataset
@@ -24,14 +25,15 @@ class LGGDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        image = io.imread(self.df.image_path[idx])[:, :, 1].astype(np.float32)/255
+        image = io.imread(self.df.image_path[idx])[:, :].astype(np.float32)/255
+        
         mask = io.imread(self.df.mask_path[idx]).astype(np.float32)/255
 
-        image = np.expand_dims(image, 0)
+        image=image.transpose(2,0,1)
+        
         mask = np.expand_dims(mask, 0)
 
         if self.transform:
             image, mask = self.transform((image, mask))
-
-        return torch.from_numpy(image), torch.from_numpy(mask)
-
+            
+        return torch.from_numpy(image),torch.from_numpy(mask)
